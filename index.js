bf0d988394e768ccc620cca17e0e85e963394aff
nicosantarin <nicosantarin@hotmail.com>
2018-02-07 07:24:24 -0800

    added lab3

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
+//using cmd prompt 'npm install'
+//'node index.js' to start server
 
 
-// This is project entry point. If you start the
-// server by typing node expresstut.js and then open the
-// browser at localhost:3000 you'll get a 404 error if
-// you haven't defined any routes
-// Imports
+// Import the express module
 var express = require('express');
-var bodyParser = require('body-parser'); 
+//imports
+var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-//imported filesystem
-var fs = require('file-system');
-
 var app = express();
 
 var router = express.Router()
@@ -19,76 +15,76 @@ var controlRoute = express.Router()
 
 app.disable('x-powered-by');
 
+
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
 
- 
+//imported filesystem
+var fs = require('file-system');
 
-
+ // random num generator
+ var rNum = Math.floor((Math.random() * 999) + 1);
+ let rData = {
+    data: rNum
+};
 // //write to json
-// let data = JSON.stringify(rData);  
-// fs.writeFileSync('./realtimedb.json', data);  
+// let data = JSON.stringify(rData);
+// fs.writeFileSync('./realtimedb.json', data);
 
 
- //global variable of json data
-app.locals.realtimedbObj = require('./realtimedb.json')
+//  //global variable of json
+// app.locals.realtimedbObj = require('./realtimedb.json')
+
+//load json file
+var userDB = fs.readFileSync('./usersDB.json','utf8');
+userDB = JSON.parse(userDB);
 
 
 
 
 
-
-
-// fs.mkdir('1/2/3/4/5', [0o777], function(err) {});
-// fs.mkdirSync('1/2/3/4/5', [0o777]);
-// fs.writeFile('path/test.txt', 'aaa', function(err) {})
-
-//using RESTful
-//routing request to add user
-//app.post('/users/:userid',1001);
- 
-
-
+//more imports
 //define port to run on
 app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(__dirname + '/public'));
 
-
 app.use('/', router);
+
 //middleware bodyparser
 router.use(bodyParser.json())
- 
 
-// route to home.handlebars
+
+// lab 3 route
 router.get('/', (req, res) => {
-  res.render('home') 
+  res.render('home')
 });
 
 
 
-// route realtimep.handlebars - go realtime > show
+
+// go realtime > show
 router.use('/realtime', realtimeRoute )
-
-
-//access /realtime/show
-realtimeRoute.get('/show', (req, res) => {
+realtimeRoute.get('/reactshow', (req, res) => {
+//   //write to json
+// let data = JSON.stringify(rData);
+// fs.writeFileSync('./realtimedb.json', data);
    res.render('realtimep')
 });
 
  //access /realtime/data
 realtimeRoute.get('/data', (req, res) => {
 
- // random number generator
- var rNum = Math.floor((Math.random() * 999) + 1);
- let rData = {  
-    data: rNum
-};
-//write random data number to json when is page accessed
-let data = JSON.stringify(rData);  
-fs.writeFileSync('./realtimedb.json', data);  
+  randNum = Math.floor((Math.random() * 999) + 1);
+  res.json({"data": randNum });
 
-//load and parse JSON database to a variable
+
+
 var text = fs.readFileSync('./realtimedb.json','utf8')
 text = JSON.parse(text)
 //console.log (typeof text)
@@ -97,14 +93,53 @@ res.send(text)
 
 
 
-//route to control page
+//go to users control.
 router.use('/users', controlRoute )
-// access /control/
-controlRoute.get('/control', (req, res) => {
+controlRoute.get('/reactcontrol', (req, res) => {
 
 	res.render('usersp')
 });
- 
+
+
+//PUT
+app.put('/users', function(req, res){
+	var body = req.body;
+	console.log(body);
+	res.send("PUT request success");
+});
+
+app.get('/users', function(req, res){
+	console.log ("GET: is a " + typeof userDB + " type");
+	res.send(userDB);
+});
+
+// POST - Update json using the form from /users/reactcontrol
+app.post('/users/submit', function(req, res){
+	var nBody = req.body;
+	var newid = Number(nBody.id);
+	userDB['theUsers'].push(nBody);
+	userDBstringObj = JSON.stringify(userDB, null, 2);
+	/*if(users[id]){
+		var reply = {
+			msg: "ID already exists."
+		}
+	}else{
+	users[id] = firstname + " " + lastname;*/
+
+	nBody = JSON.stringify(userDB, null, 2);
+	fs.writeFile('./usersDB.JSON', userDBstringObj, function (err) {
+		if (err) throw err;
+
+		console.log('Updated!');
+    // console.log(userDB);
+		res.send("User ID: " + nBody + " created Successfully!");
+	});
+});
+//DELETE
+app.delete('/users/:id', function(req, res){
+	console.log ("DELETE: ");
+	res.send(userDB);
+});
 
 
 
@@ -118,6 +153,7 @@ app.use(function(req, res) {
 // Point to 404.handlebars view
   res.render('404');
 });
+
 
 
 app.listen(app.get('port'), function(){
